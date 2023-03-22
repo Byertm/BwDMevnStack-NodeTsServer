8e2f09caf6fa5439a861122f8c19fa925c6892aa
Ersin Biltekin <ersin.biltekin.57@gmail.com>
2023-03-22 03:07:52 +0300

    Public Assets Js Library Name Changed, Stil Css Edited

--- public/assets/js/bwd-dashboards.1.3.2.js
+++ public/assets/js/bwd-dashboards.1.3.2.js
@@ -10,9 +10,9 @@
 }(this, (function () {
 	'use strict';
 
-	if (typeof Chart === 'undefined') {
-		throw new Error('BwDServer Admin requires the Chart.js library in order to function properly.');
-	}
+	// if (typeof Chart === 'undefined') {
+	// 	throw new Error('BwDServer Admin requires the Chart.js library in order to function properly.');
+	// }
 
 	window.BwDAdmin = window.BwDAdmin ? window.BwDAdmin : {};
 
@@ -25,40 +25,35 @@
 	/**
 	 * Chart.js - Line Chart with Vertical Line
 	 */
-	Chart.defaults.LineWithLine = Chart.defaults.line;
-	Chart.controllers.LineWithLine = Chart.controllers.line.extend({
-		draw: function draw(ease) {
-			Chart.controllers.line.prototype.draw.call(this, ease);
-			if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
-				let activePoint = this.chart.tooltip._active[0],
-					ctx = this.chart.ctx,
-					x = activePoint.tooltipPosition().x,
-					topY = this.chart.scales['y-axis-0'].top,
-					bottomY = this.chart.scales['y-axis-0'].bottom;
+	// Chart.defaults.LineWithLine = Chart.defaults.line;
+	// Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+	// 	draw: function draw(ease) {
+	// 		Chart.controllers.line.prototype.draw.call(this, ease);
+	// 		if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+	// 			let activePoint = this.chart.tooltip._active[0],
+	// 				ctx = this.chart.ctx,
+	// 				x = activePoint.tooltipPosition().x,
+	// 				topY = this.chart.scales['y-axis-0'].top,
+	// 				bottomY = this.chart.scales['y-axis-0'].bottom;
 
-				// Draw the line
-				ctx.save();
-				ctx.beginPath();
-				ctx.moveTo(x, topY);
-				ctx.lineTo(x, bottomY);
-				ctx.lineWidth = 0.5;
-				ctx.strokeStyle = '#ddd';
-				ctx.stroke();
-				ctx.restore();
-			}
-		}
-	});
+	// 			// Draw the line
+	// 			ctx.save();
+	// 			ctx.beginPath();
+	// 			ctx.moveTo(x, topY);
+	// 			ctx.lineTo(x, bottomY);
+	// 			ctx.lineWidth = 0.5;
+	// 			ctx.strokeStyle = '#ddd';
+	// 			ctx.stroke();
+	// 			ctx.restore();
+	// 		}
+	// 	}
+	// });
 
 	$(document).ready(function () {
-
 		/**
 		 * Dropdown adjustments
 		 */
-
-		let slideConfig = {
-			duration: 270,
-			easing: 'easeOutSine'
-		};
+		let slideConfig = { duration: 270, easing: 'easeOutSine' };
 
 		// Add dropdown animations when toggled.
 		$(':not(.main-sidebar--icons-only) .dropdown').on('show.bs.dropdown', function () {
